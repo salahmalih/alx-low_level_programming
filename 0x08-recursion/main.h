7344b17d335d8e2e81b15af202ef67043915b900
salahmalih <C130084552@taalim.ma>
2023-11-15 14:54:19 -0600

    some edite

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
-void _int _putchar(char c);
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,4 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
 #endif

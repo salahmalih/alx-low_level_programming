0bd657b2f9a34942027840dd87a0ed8710a3fc86
salahmalih <C130084552@taalim.ma>
2023-11-15 18:43:02 +0100

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts recursion - Write a function that prints a string
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s){
+	int i=1;
+	if(!s){
+		_putchar(*s);
+		_puts_recursion(s+i);
+	}
+	else{
+		_putchar('\n');
+	}
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+void _puts_recursion(char *s);
+int _putchar(char c);
+#endif

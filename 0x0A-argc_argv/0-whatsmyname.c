853ee983ad435824dfd426320cfe5ac7f9954074
salahmalih <C130084552@taalim.ma>
2023-11-17 14:48:04 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,9 @@
 /**
  * main - Write a program that prints its name
  *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc __attribute__((unused)), char* argv[]){

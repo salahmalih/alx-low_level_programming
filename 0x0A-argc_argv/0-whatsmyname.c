a9cfb3cc2fecb439fa78c39a6fa374d7c8e44354
salahmalih <C130084552@taalim.ma>
2023-11-17 14:41:57 +0100

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char* argv[]){
+	printf("%s\n",*argv);
+	return (0);
+}

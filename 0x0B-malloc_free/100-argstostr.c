d613ba3a0af30ec823ec791c0ef357d5d87ac434
salahmalih <C130084552@taalim.ma>
2023-11-20 15:16:18 -0600

    some edite

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,83 +1,50 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * _strncpy - copies a string
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to copy
- *
- * Return: pointer to the resulting string
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
-
-	i = 0;
-
-	while (src[i] != '\0' && i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-
-	return (dest);
-}
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-/**
-*argstostr - concatenates all arguments of the program.
-*@ac: argument count.
-*@av: pointer to array of size ac.
-*Return: NULL if ac == 0 or av == null, Pointer to new string.
-*NULL on fail.
-*/
+  *argstostr - concatenates all arguments of the program.
+  *@ac: argument count.
+  *@av: pointer to array of size ac.
+  *Return: NULL if ac == 0 or av == null, Pointer to new string.
+  *NULL on fail.
+  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, len;
-	char *arc;
+	int i, j, k, size;
+	char *arg;
 
+	size = 0;
+	k = 0;
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-		len += _strlen(av[i]);
-	arc = (char *)malloc(len + 1);
-	if (arc == NULL)
+	i = 0;
+	while (i < ac)
 	{
-		free(arc);
+		j = 0;
+		while (av[i][j])
+		{
+			size++;
+			j++;
+		}
+		size++;
+		i++;
+	}
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
 		return (NULL);
-	}
-	j = 0;
-	for (i = 0; i < ac; i++)
+	i = 0;
+	while (i < ac)
 	{
-		_strncpy(arc + j, av[i], ac);
-		j += _strlen(av[i]);
-		arc[j++] = '\n';
+		j = 0;
+		while (av[i][j])
+		{
+			arg[k] = av[i][j];
+			j++;
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+		i++;
 	}
-	arc[j] = '\0';
-	return (arc);
+	arg[k] = '\0';
+	return (arg);
 }

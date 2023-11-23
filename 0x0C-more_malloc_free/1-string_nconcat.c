44c7d8bdeed7f719ba6dcabd6e1ee8826c163ae1
salahmalih <C130084552@taalim.ma>
2023-11-23 09:17:46 -0600

    some edite

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,40 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
- * *string_nconcat - function that concatenates two strings.
- * @s1:String 1;
- * @s2:String 2;
- * @n:lenght.
+ * string_nconcat - Concatenates two strings up to the specified length.
+ * @s1: First string.
+ * @s2: Second string.
+ * @n: Maximum number of characters to concatenate from s2.
  *
- * Return: concatenates.
+ * Return: A newly allocated string containing s1 followed by the first
+ * n characters of s2, followed by a null terminator.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len;
-	unsigned int j, l;
+	unsigned int len1 = 0, len2 = 0;
+	unsigned int i, j;
 	char *str;
 
-	len = 0;
-	i = 0;
-	while (s1[len] != '\0')
-		len++;
-	while (s2[i] != '\0')
-		i++;
-	if (n >= i)
-		n = i;
-	str = malloc(sizeof(char) * (len + n + 1));
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
+	if (n >= len2)
+		n = len2;
+	str = malloc(sizeof(char) * (len1 + n + 1));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len1; i++)
 	{
 		str[i] = s1[i];
 	}
-	for (j = i, l = 0; j < (n + len); j++, l++)
+	for (j = 0; j < n; j++, i++)
 	{
-		str[j] = s2[l];
+		str[i] = s2[j];
 	}
-	str[n + len] = '\0';
+	str[i] = '\0';
+
 	return (str);
 }

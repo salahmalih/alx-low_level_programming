a3d1a53cda91827b421a8c42266bd05d4498186d
salahmalih <C130084552@taalim.ma>
2023-11-23 10:04:25 -0600

    some edite

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include "main.h"
-char *_setZero(char *s, char b, unsigned int n);
 /**
- * _calloc - Allocates memory for an array of
- * nmemb elements, each of size bytes.
+ * _calloc - Allocates memory for an array.
+ *
  * @nmemb: Number of elements.
  * @size: Size of each element in bytes.
  *
@@ -12,34 +11,17 @@ char *_setZero(char *s, char b, unsigned int n);
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int i;
+	char *ptr;
 
 	if (nmemb == 0 && size == 0)
 		return (NULL);
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
-	_setZero(ptr, 0, nmemb * size);
-	return (ptr);
-}
-
-/**
- * _setZero - Sets the first n bytes of
- * the memory area pointed to by s to zero.
- * @s: Pointer to the memory area.
- * @b: The byte to set.
- * @n: Number of bytes to set to the value.
- *
- * Return: A pointer to the memory area s.
- */
-char *_setZero(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
-		s[i] = b;
+		ptr[i] = 0;
 	}
-
-	return (s);
+	return (ptr);
 }

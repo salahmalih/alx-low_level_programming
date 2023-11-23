af4536bfd1ea2171a85b8822928081b557977646
salahmalih <C130084552@taalim.ma>
2023-11-23 08:43:45 -0600

    some edite

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,6 +24,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	if (n >= i)
 		n = i;
+	if (i == 0)
+		return (s1);
+	if (len == 0)
+		return (s2);
 	str = malloc(sizeof(char) * (len + n + 1));
 	if (str == NULL)
 		return (NULL);

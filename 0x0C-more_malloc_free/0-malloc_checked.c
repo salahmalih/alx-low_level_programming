d22eec06559b3745282dc46deb9c88c7b118c048
salahmalih <C130084552@taalim.ma>
2023-11-23 12:50:31 +0100

    update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - function that allocates memory using malloc
+ *
+ * @b: number of bytes to allocate
+ *
+ * Return:a pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+
+	c = malloc(b);
+	if (c == NULL)
+		exit(98);
+	return c;
+}

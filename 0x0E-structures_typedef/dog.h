d14c64c86fe129762e770109b689923837eac228
salahmalih <C130084552@taalim.ma>
2023-11-27 09:31:41 -0600

    some edite

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,8 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-typdef struct dog dog_t;
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif

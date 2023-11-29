5528bcb4231b7181e36aa74203f5716debbb6566
salahmalih <C130084552@taalim.ma>
2023-11-29 10:50:24 -0600

    fix betty 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
 #ifndef CALC_H
-#define CLAC_H
+#define CALC_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,8 +13,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -23,4 +23,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif

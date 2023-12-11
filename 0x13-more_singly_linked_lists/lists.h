9967798cd167dbf7416e9d94076612d0ca0d331e
salahmalih <C130084552@taalim.ma>
2023-12-11 18:06:53 +0100

    add task 3 and 4

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,4 +20,6 @@ typedef struct listint_t
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t listint_len(const listint_t *h);
+void free_listint(listint_t *head);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif

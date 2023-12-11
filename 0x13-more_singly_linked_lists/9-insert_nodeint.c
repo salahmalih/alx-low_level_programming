e01038156fa0c43570b764169047fc1bc0c4ec01
salahmalih <C130084552@taalim.ma>
2023-12-11 16:12:55 -0600

    betty code c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,13 +27,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ind = (*head);
 	while (ind && i < idx)
 	{
-		ind = ind->next;
-		if (i == idx -1)
+		if (i == (idx - 1))
 		{
 			new->next = ind->next;
 			ind->next = new;
 			return (new);
 		}
+		else
+			ind = ind->next;
 		i++;
 	}
 

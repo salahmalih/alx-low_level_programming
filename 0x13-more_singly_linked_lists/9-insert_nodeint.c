af492e83b98b7b3bd333786731755aab4b446da5
salahmalih <C130084552@taalim.ma>
2023-12-11 16:28:33 -0600

    betty code c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,8 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *new, *ind;
+	listint_t *new;
+	listint_t *ind;
 
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)

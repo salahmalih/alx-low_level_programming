0e5f62e9ed5ce73efacc9e30e18f9ca952ca37f2
salahmalih <C130084552@taalim.ma>
2023-12-14 17:06:34 +0100

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,19 +4,21 @@
  * print_binary - print binary representation of a number
  * @n: decimal number to print as binary
  */
-void print_binary(unsigned long int n) {
-
+void print_binary(unsigned long int n)
+{
 	unsigned long int mask;
 	int leading_zeros = 1;
 
 	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	if (n == 0) {
+	if (n == 0)
+	{
 		printf("0");
 		return;
 	}
 	while (mask > 0)
 	{
-		if (n & mask) {
+		if (n & mask)
+		{
 			leading_zeros = 0;
 			printf("1");
 		}
